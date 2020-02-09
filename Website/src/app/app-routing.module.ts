712e069e28d84dcd541276dad646ef9c981cb560
Joseph Jordan <joe.jordan1996@gmail.com>
2020-02-09 12:35:18 +0000

    Added routing for Angular app, and a Postman collection for testing.

--- Website/src/app/app-routing.module.ts
+++ Website/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoanComponent } from './loan/loan.component';
+import { LoansComponent } from './loans/loans.component';
+import { LoanAddEditComponent } from './loan-add-edit/loan-add-edit.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoansComponent, pathMatch: 'full' },
+  { path: 'loan/:id', component: LoanComponent },
+  { path: 'add', component: LoanAddEditComponent },
+  { path: 'loan/edit/:id', component: LoanAddEditComponent },
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
