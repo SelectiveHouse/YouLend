d237ffb36ea4b7543c404584f58523abb6c2616e
Joseph Jordan <joe.jordan1996@gmail.com>
2020-02-09 17:34:48 +0000

    Adding to README, preparing add/edit.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[TOC]
-
 # YouLend Test Project
 
 Hi Mark, or whoever is reading this, this was a really fun project to work on :)
@@ -8,17 +6,31 @@ I'll run through each of the folders and how to setup/work them correctly.
 
 If you have any questions please ask me.
 
-## Overview
+## Architectural Overview
 
 ![YouLend Comp Diagram](https://tcvplq.am.files.1drv.com/y4me9FCKh4k6b9-4OIoUUGnkrNr8zmI_V-qTtQRHtu8BHXE04FcQxhTUxryXYC7-gthSUnlZfSYJ-NoV2DtUg775qfnna_4pIU5Q3kFnrMd4-vRcEE_9SWq4rh_roM3L7NYDRZ6QPvG4DMsTr_SbReijThOPdtlRYuRBu-YcunU26jaDc2eX6Zc0qK_bYuy41qiu3XmQpFeoeJWxVc9zLIYsw?width=1760&height=1360&cropmode=none)
 
 ## Libraries and Software Used
 
+- Visual Studio 2019 Version 16.4.4
+- Visual Studio Code Version 1.42.0
+- Postman Version 7.17.0
+- Node.js / NPM 13.7.0
+- Bootstrap 4.4.1
+- Angular 9.0.0
+- ASP.NET Core 3.1
 
+For a complete list of **Server-Side** packages, see YouLend/YouLend.WebApi/YouLend.WebAPI.csproj
+
+For a complete list of **Client-Side** packages, see YouLend/Website/package.json
 
 ## Building the Project
 
+### Server
 
+Open the 
+
+### Client
 
 ## Project Structure
 
--- Website/src/app/loan-add-edit/loan-add-edit.component.ts
+++ Website/src/app/loan-add-edit/loan-add-edit.component.ts
@@ -10,44 +10,15 @@ import { Loan } from '../models/loan';
   styleUrls: ['./loan-add-edit.component.scss']
 })
 export class LoanAddEditComponent implements OnInit {
-  form: FormGroup;
-  actionType: string;
-  formTitle: string;
-  loanId: number;
-  errorMsg: any;
-  existingLoan: Loan;
-
   constructor(private loanService: LoanService, private formBuilder: FormBuilder, private avRoute: ActivatedRoute, private router: Router) {
-      const idParam = 'id';
-      this.actionType = 'Add';
-      this.formTitle = 'borrowerFullName';
-      if (this.avRoute.snapshot.params[idParam]) {
-        this.loanId = this.avRoute.snapshot.params[idParam];
-      }
-
-      this.form = this.formBuilder.group(
-        {
-          loanId: 0,
-          title: ['', [Validators.required]],
-          body: ['', [Validators.required]]
-        }
-      );
+    
   }
 
   ngOnInit() {
-    if (this.loanId > 0) {
-      this.actionType = 'Edit',
-      this.loanService.getLoan(this.loanId)
-        .subscribe(data => (
-          this.existingLoan = data,
-          this.form.controls[this.formTitle].setValue(data.borrowerFullName)
-        ));
-    }
   }
 
-  save() {
-    if (!this.form.valid) {
-      return;
-    }
+  cancel() {
+    this.router.navigate(['/']);
   }
+
 }
